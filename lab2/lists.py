96f9bbdd313df52fee3396696b2db668781bedb2
Merekezhan <m_zhandarbek@kbtu.kz>
2024-01-26 13:13:50 +0600

    first commit

---
+++ lab2/lists.py
@@ -0,0 +1,21 @@
+# ex1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+# ex2
+fruits = ["apple", "banana", "cherry"]
+fruits[0]="kiwi"
+
+# ex3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+# ex4
+
+# ex5
+
+# ex6
+
+# ex7
+
+# ex8
