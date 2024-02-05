51f7c0cbdc890a3d22172d26a1c156da4d3e0ec5
Merekezhan <m_zhandarbek@kbtu.kz>
2024-02-05 22:35:21 +0600

    first commit

--- lab3/inheritance.py
+++ lab3/inheritance.py
@@ -1,16 +1,16 @@
-# ex1
-class Student(Person):
+# # ex1
+# class Student(Person):
 
 # ex2
-    class Person:
-        def __init__(self, fname):
-            self.firstname = fname
+class Person:
+    def __init__(self, fname):
+        self.firstname = fname
 
-        def printname(self):
-            print(self.firstname)
+    def printname(self):
+        print(self.firstname)
 
-    class Student(Person):
-        pass
+class Student(Person):
+    pass
 
 x = Student("Mike")
 x.printname()
