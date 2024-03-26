e1e7be36cb8e4080e9cd2f48c4a72c4146076677
Merekezhan <m_zhandarbek@kbtu.kz>
2024-03-26 13:29:41 +0600

    first commit

---
+++ draft/aftersec.py
@@ -0,0 +1,11 @@
+from time import sleep
+import math
+
+def sq(num, m):
+    sleep(m/1000)
+    x = math.sqrt(num)
+    print(x)
+
+num = int(input())
+m = int(input())
+sq(num, m)
--- draft/regex.py
+++ draft/regex.py
@@ -1,2 +1,14 @@
-import os
-print(os.getcwd())
+"""1 - Write a Python program that matches a string that has an 'a' followed by zero or more 'b''s"""
+
+import re
+
+file=open("row.txt", "r", encoding="utf8")
+text= file.read()
+
+if re.search('a(b*)', text):
+    print("matches")
+else:
+    print("no matches")
+
+
+ 
